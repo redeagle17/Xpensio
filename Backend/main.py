41e10f397d7bafa450448847661ae2f12bbc2a4a
Ankur Singh <92366204+redeagle17@users.noreply.github.com>
2022-12-27 19:45:30 +0530

    before deployment

--- Backend/main.py
+++ Backend/main.py
@@ -11,27 +11,51 @@ import pandas as pd
 db=firestore.client()
 app = FastAPI()
 
-li = ["Dear SBI User, your A/c X4954-debited by Rs50.0 on 19Nov22 transfer to Dunzo Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs55.0 on 19Nov22 transfer to Dunzo Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs60.0 on 19Nov22 transfer to Dunzo Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs65.0 on 19Nov22 transfer to Dunzo Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs650.0 on 19Nov22 transfer to Dunzo Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs1000.0 on 19Nov22 transfer to easemytrip Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs6550.0 on 19Nov22 transfer to zomato Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs2000.0 on 19Nov22 transfer to easemytrip Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs10000.0 on 19Nov22 transfer to zomato Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs20000.0 on 19Nov22 transfer to zomato Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI",
-"Dear SBI User, your A/c X4954-debited by Rs20000.0 on 19Nov22 transfer to easemytrip Digital Ref No 232316221258. If not done by u, fwd this SMS to 9223008333/Call 1800111109 or 09449112211 to block UPI -SBI"]
+# li=["Dear SBI User, your Ac X3380-debited by Rs90.0 on 24Dec22 transfer to Aradhna Ref No 235868808086. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs5.0 on 23Dec22 transfer to Sameer ahmed Ref No 235849801277. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs20.0 on 22Dec22 transfer to Sri Hanuman Rice Ref No 235835371020. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your A X3380-debited by Rs76.0 on 21Dec22 transfer to KSHITIJ VERMA Ref No 235812432203. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs35.0 on 20Dec22 transfer to SHALAM AGNU MIYA Ref No 235547425203. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs366.0 on 19Dec22 transfer to ARNAV RAHUL GHULE Ref No 235310911743. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs40.0 on 18Dec22 transfer to SHANI Ref No 235168438606. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs75.0 on 17Dec22 transfer to Aryan Singh Ref No 235290721453. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs190.0 on 16Dec22 transfer to SAMYAK BHABHRA Ref No 235105794084. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs65.0 on 15Dec22 transfer to EURONETGPAY Ref No 235041860831. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs35.0 on 14Dec22 transfer to ARNAV RAHUL GHULE Ref No 235009147014. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs307.0 on 13Dec22 transfer to RAJU TATU RATHOD Ref No 234899349010. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs10.0 on 12Dec22 transfer to Kumary manjula Ref No 234778018951. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs30.0 on 11Dec22 transfer to MANIK Ref No 234654657572. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs10.0 on 10Dec22 transfer to SAMEER AHMED Ref No 234537410978. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs329.0 on 09Dec22 transfer to AMAZON SELLER Ref No 234056993483. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs9800.0 on 08Dec22 transfer to MANJULA K C Ref No 233902929109. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs28.0 on 07Dec22 transfer to SREE HANUMAN Ref No 233825590869. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs147.0 on 06Dec22 transfer to KSHITIJ VERMA Ref No 233630672311. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs306.0 on 05Dec22 transfer to Dominos Pizza Ref No 233493861442. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs25.0 on 04Dec22 transfer to Sameer ahmed Ref No 233347503397. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs10.0 on 03Dec22 transfer to Sameer ahmed Ref No 233274650174. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs20.0 on 02Dec22 transfer to Sameer ahmed Ref No 233057131501. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs650.0 on 01Dec22 transfer to Kaggis Cakes And Ref No 232711006724. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI",
+# "Dear SBI User, your Ac X3380-debited by Rs1000.0 on 01Dec22 transfer to Kaggis Cakes And Ref No 232711006724. If not done by u, fwd this SMS to 9223008333Call 1800111109 or 09449112211 to block UPI -SBI"]
 
-@app.get('/signup')
-async def status(q:str):
+@app.get('/signup/{q}/{t}')
+async def status(q:str,t:str):
+    li=[]
+    t = t.split("\", ")
+    for item in t:
+        li.append(item)
+    li.reverse()
     db.collection(q).document("Message List").collection("List").document('List').set({'list':li})
     findstring(li,q)
     x=cat(q)
     return x
 
-@app.get('/login')
-async def login(q:str):
+@app.get('/login/{q}/{t}')
+async def login(q:str,t:str):
+    li=[]
+    t = t.split("\", ")
+    for item in t:
+        li.append(item)
+    li.reverse()
     old=db.collection(q).document("Message List").collection("List").get()
     new_list=[]
     for i in old:
@@ -46,20 +70,28 @@ async def login(q:str):
 
 @app.get('/predict')
 async def predict():
-    # trans=db.collection("Email").document("Transactions").collection("Transaction").get()
-    # date=[]
-    # amount=[]
-    # for price in trans:
-    #     d=list(price.to_dict().values())
-    #     date.append(d[1])
-    #     amount.append(d[2])
-    # x=arima_prediction(date,amount)
-    # df=pd.read_csv('Electric_Production.csv')
     doc=db.collection("Email").document("Predictions_of_next_month").collection("Predictions").get()
     if len(doc)>0:
         y=db.collection("Email").document("Predictions_of_next_month").collection("Predictions").document('Prediction').get()
         ans=list(y.to_dict().values())
         return ans[0]
-    x=arima_prediction()
-    # return x
-    db.collection("Email").document("Predictions_of_next_month").collection("Predictions").document('Prediction').set({'Prediction':x})
+    else:
+        trans=db.collection("Email").document("Transactions").collection("Transaction").get()
+        date=[]
+        amount=[]
+        for d in trans:
+            d=doc.to_dict()
+            a=d.get('Amount')
+            c=d.get('Category')
+            date.append(c)
+            amount.append(a)
+        x=arima_prediction(date,amount)
+        return x
+        # db.collection("Email").document("Predictions_of_next_month").collection("Predictions").document('Prediction').set({'Prediction':x})
+
+
+
+
+
+
+
--- Backend/prediction.py
+++ Backend/prediction.py
@@ -4,10 +4,11 @@ import statsmodels.tsa.stattools as tsa
 from statsmodels.tsa.arima.model import ARIMA
 from datetime import datetime
 # def arima_prediction(date,amount):
-#     df = pd.DataFrame(list(zip(date,amount)),columns =['Date', 'Expense'])
+    
 df=pd.read_csv('Electric_Production.csv')
-def arima_prediction():
-    df.rename(columns = {'IPG2211A2N':'Production'}, inplace = True)
+def arima_prediction(date,amount):
+    df = pd.DataFrame(list(zip(date,amount)),columns =['DATE', 'Expense'])
+    # df.rename(columns = {'IPG2211A2N':'Production'}, inplace = True)
     df['DATE']=pd.to_datetime(df['DATE'])
     df.set_index('DATE',inplace=True)
     x=len(df.axes[0])
@@ -18,7 +19,7 @@ def arima_prediction():
     def MAPE(Y_actual,Y_Predicted):
         mape = np.mean(np.abs((Y_actual - Y_Predicted)/Y_actual))*100
         return mape
-    r=adf_test(df['Production'])
+    r=adf_test(df['Expense'])
     flag=0
     d=0
     if(r<=0.05):
@@ -26,7 +27,7 @@ def arima_prediction():
         flag=1
     d=d+1
     while flag==0:
-        r=adf_test((df['Production']-df['Production'].shift(d)).dropna())
+        r=adf_test((df['Expense']-df['Expense'].shift(d)).dropna())
         if(r<=0.5): break
         d=d+1
     train=df[:new_x]
@@ -39,18 +40,18 @@ def arima_prediction():
     q=[1,2,3,4,5,6,7,8]
     for i in p:
         for j in q:
-            model_ARIMA=ARIMA(train['Production'],order=(i,d,j))
+            model_ARIMA=ARIMA(train['Expense'],order=(i,d,j))
             model_Arima_fit=model_ARIMA.fit()
             pred_start_date=test.index[0]
             pred_end_date=test.index[-1]
             pred=model_Arima_fit.predict(start=pred_start_date,end=pred_end_date)
-            mape=MAPE(test['Production'],pred)
+            mape=MAPE(test['Expense'],pred)
             if mape<=0.5 and mape<score:
                 score=mape
                 new_p=i
                 new_q=j
 
-    new_model_ARIMA=ARIMA(df['Production'],order=(new_p,d,new_q))
+    new_model_ARIMA=ARIMA(df['Expense'],order=(new_p,d,new_q))
     new_model_ARIMA_fit=new_model_ARIMA.fit()
     pred=new_model_ARIMA_fit.forecast(1)
     ans=pred[0]
